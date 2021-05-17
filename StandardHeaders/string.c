f6605e4bab7234db53f7af7f1ca005fdfd0afd6c
pentolope <pentolope@gmail.com>
2021-05-17 17:31:37 -0600

    lib functions
    added strcmp and memchr to string.h and string.c

--- StandardHeaders/string.c
+++ StandardHeaders/string.c
@@ -97,12 +97,28 @@ static char* strchr(const char* str,int character){
 	while (1){
 		c=*str;
 		if (c==character) return (char*)str;
-		if (c==0) return NULL;
+		if (c==0) return (void*)0;
 		str++;
 	}
 }
 
+static int strcmp(const char* str1,const char* str2){
+	while (1){
+		char c1;char c2;
+		c1=*(str1++);
+		c2=*(str2++);
+		if (c1==c2 & c1!=0) continue;
+		return c1-c2;
+	}
+}
 
-
+static void* memchr(const void* ptr, int value, unsigned long num){
+	unsigned char* ptr0=(unsigned char*)ptr;
+	while (num--!=0){
+		unsigned char* ptr1=ptr0;
+		if ((unsigned char)value==*(ptr0++)) return ptr1;
+	}
+	return (void*)0;
+}
 
 
--- StandardHeaders/string.h
+++ StandardHeaders/string.h
@@ -10,6 +10,8 @@ static void memset(void*,int v,unsigned long);
 static void memcpy(void*,const void*,unsigned long);
 static void strcpy(char*,const char*);
 static char* strchr(const char*,int);
+static int strcmp(const char* str1,const char* str2);
+static void* memchr(const void* ptr, int value, unsigned long num);
 
 #else // #ifdef __STD_REAL
 
@@ -18,6 +20,8 @@ extern void memset(void*,int v,unsigned long);
 extern void memcpy(void*,const void*,unsigned long);
 extern void strcpy(char*,const char*);
 extern char* strchr(const char*,int);
+extern int strcmp(const char* str1,const char* str2);
+extern void* memchr(const void* ptr, int value, unsigned long num);
 
 #endif // #ifdef __STD_REAL
 
